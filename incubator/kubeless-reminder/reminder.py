e58e01b57b86d8ebd17e0c2814b0e6bdc3ad65d5
Tuna <ng.tuna@gmail.com>
2017-12-14 09:49:55 +0700

    update access link: switch to use zoom

--- incubator/kubeless-reminder/reminder.py
+++ incubator/kubeless-reminder/reminder.py
@@ -19,7 +19,17 @@ def remind():
                        "chat.postMessage",
                        channel="#kubeless",
                        text="""<!here> please jump into the weekly call for discussing things going on Kubeless.
-Link to access: https://meet.google.com/rbr-gcjp-xxz
+                       
+Join from PC, Mac, Linux, iOS or Android: https://zoom.us/j/536180327
+
+Or iPhone one-tap :
+    US: +16699006833,,536180327#  or +16465588656,,536180327#
+Or Telephone:
+    Dial(for higher quality, dial a number based on your current location)：
+        US: +1 669 900 6833  or +1 646 558 8656
+    Meeting ID: 536 180 327
+    International numbers available: https://zoom.us/zoomconference?m=H8zcNnFc19vCkVdoEHBkxK61gwDvyawC
+
 Meeting notes: https://docs.google.com/document/d/1-OsikjjQVHVFoXBHUbkRogrzzZijQ9MumFpLfWCCjwk/edit?usp=sharing
                        """,
                        as_user=True
