21cf2bc9372fce355ce22f26b39973eea6e41ec8
Andres <andres@bitnami.com>
2018-03-02 11:38:49 +0100

    Simplify function

--- incubator/weather/handler.js
+++ incubator/weather/handler.js
@@ -9,14 +9,10 @@ module.exports = {
         if (!location) {
             throw new Error('You must provide a location.');
         }
-        try {
-            const response = await rq(`https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="${location}") and u="c"&format=json`);
-            const condition = JSON.parse(response).query.results.channel.item.condition;
-            const text = condition.text;
-            const temperature = condition.temp;
-            res.end(`It is ${temperature} celsius degrees in ${location} and ${text}`)
-        } catch (e) {
-            throw e;
-        }
+        const response = await rq(`https://query.yahooapis.com/v1/public/yql?q=select item.condition from weather.forecast where woeid in (select woeid from geo.places(1) where text="${location}") and u="c"&format=json`);
+        const condition = JSON.parse(response).query.results.channel.item.condition;
+        const text = condition.text;
+        const temperature = condition.temp;
+        res.end(`It is ${temperature} celsius degrees in ${location} and ${text}`)
     }
 }
